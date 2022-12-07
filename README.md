4814cbc50b4792689ef0d3bd40302a68fc7a2fbb
Jonathan Foucher <jfoucher@gmail.com>
2022-12-08 00:33:21 +0100

    Midi over USB works

--- README.md
+++ README.md
@@ -1,18 +1,3 @@
-# PICOVIC
+# SEEQ
 
-This is a Commodore Vic-20 emulator for the [Raspberry Pi Pico](https://www.raspberrypi.org/products/raspberry-pi-pico/)
-
-![Screen photo](basic.png)
-
-Most of the 6502 emulation code is from [fake6502](http://rubbermallet.org/fake6502.c).
-
-A PS/2 keyboard can be plugged in to the pico according to [the schematic](hardware/picovic.pdf) and is used as an input device.
-
-VGA output is based on the scanvideo code from [pico-extra](https://github.com/raspberrypi/pico-extras/tree/master/src/common/pico_scanvideo)
-
-
-The emulation is extremely basic and for example the VIAs are only emulated so that the cursor works in BASIC. The VIC chip only has the most simple functionality, in the interest of speed and laziness. However since for the time being the emulated speed is higher than the original VIC-20, more functionality will be added in the future.
-
-![running basic](output.gif)
-
-[Running BASIC video](basic.mp4)
+## A RP2040 midi and CV / Gate sequencer
--- main.cpp
+++ main.cpp
@@ -1,17 +1,31 @@
-/**
- * Copyright (c) 2021 Jonathan Foucher
+/* 
+ * The MIT License (MIT)
  *
- * SPDX-License-Identifier: MIT
+ * Copyright (c) 2022 Jonathan Foucher - Six Pixels
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
  */
 
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
-
-
-
-#define PICO_SCANVIDEO_SCANLINE_BUFFER_COUNT 64
-
 
 #include "pico/stdlib.h"
 #include "pico/time.h"
@@ -25,26 +39,22 @@
 #include "lib/OLED/font/Cherry_Cream_Soda_Regular_16.h"
 #include "lib/OLED/font/Dialog_bold_16.h"
 
+#include "pico/binary_info.h"
 
 
-#define CHAR_BUFFER_ADDRESS 0x277
-#define CHAR_BUFFER_NUM_ADDRESS 0xC6
-// If this is active, then an overclock will be applied
-// #define OVERCLOCK
-
-// Delay startup by so many seconds
-// #define START_DELAY 3
+#include "bsp/board.h"
+#include "tusb.h"
 
 
-
-absolute_time_t start;
+#define CABLE_NUM 0 // MIDI jack associated with USB endpoint
+#define CHANNEL 0 // 0 for channel 1
 
 void core1_func();
 static semaphore_t video_initted;
 
 queue_t queue;
 
-uint8_t sequence[16];
+uint8_t sequence[16] = {74,78,81,86,90,93,98,102,57,61,66,69,73,78,81,85};
 uint8_t active[16];
 uint8_t first_step = 0;
 uint8_t last_step = 15;
@@ -59,43 +69,11 @@ int tempo = 120;
 
 absolute_time_t t;
 absolute_time_t last_play_time;
+absolute_time_t last_off_time;
 bool playing = true;
 bool recording = false;
 
-// uint8_t __time_critical_func(read6502)(uint16_t address) {
-//     if (address == VIA2_PORTA2) {
-//         address = VIA2_PORTA;
-//     }
-//     if (address == VIA2_T1CL && mpu_memory[VIA2_IFR]) {
-//       mpu_memory[VIA2_IFR] = 0x00;
-//     //   printf("timer int cleared by T1CL read\n");
-//     }
-//     // if (address == VIA2_PORTB || address == VIA2_PORTA) {
-//     //     return 0;
-//     // }
-//     return mpu_memory[address];
-// }
 
-// def led_display(led):
-//     # scan the keyboard matrix and turn on the correct LED
-//     global previous_led
-//     # exit if no change
-//     if (previous_led == led):
-//         return
-    
-//     #turn everything off
-//     for pin in led_row_pins:
-//         pin.value = False
-//     for pin in led_col_pins:
-//         pin.value = True
-//     # turn our led on
-//     if (led < 16 and led >= 0):
-//         # turn on the led that should currently be on according to current_led
-//         row = led // 4
-//         col = led % 4
-//         led_row_pins[row].value = True
-//         led_col_pins[col].value = False
-//     previous_led = led
 
 void init_leds() {
     for(int i = 0; i<4;i++) {
@@ -181,6 +159,7 @@ void core1_func() {
     }
 }
 
+
 void rotaryChangedCallback(encoder::Encoder * enc)
 {
     int x = enc->count();
@@ -196,73 +175,149 @@ void rotaryChangedCallback(encoder::Encoder * enc)
     queue_try_add(&queue, &tempo);
 }
 
+void midi_task(void);
 
-int main() {
-#ifdef OVERCLOCK
+/*------------- MAIN -------------*/
+int main(void)
+{
+  #ifdef OVERCLOCK
     vreg_set_voltage(VREG_VOLTAGE_1_15);
     set_sys_clock_khz(260000, true);
-#endif
-stdio_init_all();
+  #endif
+  stdio_init_all();
 
-#ifdef START_DELAY
-    for(uint8_t i = START_DELAY; i > 0; i--) {
-        printf("Starting in %d \n", i);
-        sleep_ms(1000);
-    }
-#endif
-    printf("Initializing LEDs\n");
-    init_leds();
+  board_init();
 
-    const uint PIN_A = 26;  // The A channel pin
-    const uint PIN_B = 27;  // The B channel pin
-    encoder::Encoder enc(pio0, 0, {PIN_A, PIN_B});
+  tusb_init();
 
-    queue_init(&queue, sizeof(tempo), 32);
-    queue_try_add(&queue, &tempo);
+  printf("Initializing LEDs\n");
+  init_leds();
 
-    enc.init();
-    enc.set_count(tempo/5);
-    enc.setCallback(rotaryChangedCallback);
+  const uint PIN_A = 26;  // The A channel pin
+  const uint PIN_B = 27;  // The B channel pin
+  encoder::Encoder enc(pio0, 0, {PIN_A, PIN_B});
 
-    // rotary_encoder_t *encoder = create_encoder(26, on_rotary_change);
-    // encoder->rotation = tempo;
-    // printf("Position: %d\n", encoder->rotation);
+  queue_init(&queue, sizeof(tempo), 32);
+  queue_try_add(&queue, &tempo);
 
-    playing = true;
+  enc.init();
+  enc.set_count(tempo/5);
+  enc.setCallback(rotaryChangedCallback);
+  playing = true;
 
-    printf("starting second core\n");
-    // create a semaphore to be posted when video init is complete
-    sem_init(&video_initted, 0, 1);
+  printf("starting second core\n");
+  // create a semaphore to be posted when video init is complete
+  sem_init(&video_initted, 0, 1);
 
-    // launch all the video on core 1, so it isn't affected by USB handling on core 0
-    multicore_launch_core1(core1_func);
-    // wait for initialization of video to be complete
-    sem_acquire_blocking(&video_initted);
+  // launch all the video on core 1, so it isn't affected by USB handling on core 0
+  multicore_launch_core1(core1_func);
+  // wait for initialization of video to be complete
+  sem_acquire_blocking(&video_initted);
 
-    //hookexternal(callback);
+  printf("second core started\n");
 
-    printf("second core started\n");
+  last_play_time = get_absolute_time();
+  last_off_time = get_absolute_time();
 
-    last_play_time = get_absolute_time();
+  while (1)
+  {
+    tud_task(); // tinyusb device task
+    midi_task();
+  }
 
-    while(1) {
-        t = get_absolute_time();
-        int64_t delta = absolute_time_diff_us(last_play_time, t);
-        if (playing && delta > 60000000 / tempo) {
-            last_play_time = t;
+
+  return 0;
+}
+
+void note_on(uint8_t midi_note) {
+  // Send Note On for current position at full velocity (127) on channel 1.
+  uint8_t note_on[3] = { 0x90 | CHANNEL, midi_note, 127 };
+  tud_midi_stream_write(CABLE_NUM, note_on, 3);
+}
+
+void note_off(uint8_t midi_note) {
+  // Send Note On for current position at full velocity (127) on channel 1.
+  uint8_t note_off[3] = { 0x80 | CHANNEL, midi_note, 0};
+  tud_midi_stream_write(CABLE_NUM, note_off, 3);
+}
+
+//--------------------------------------------------------------------+
+// MIDI Task
+//--------------------------------------------------------------------+
+
+void midi_task(void)
+{
+  t = get_absolute_time();
+
+  int64_t delta = absolute_time_diff_us(last_play_time, t);
+  int64_t delta_off = absolute_time_diff_us(last_off_time, t);
+
+      // elif(playing and tempo > 0 and delta > 30/tempo and delta_off > 60/tempo):
+      //   # send note off halfway to the next note
+      //   last_note_off_time = t
+        
+      //   ps = play_step - 1
+      //   if ps < 0:
+      //       ps = last_step
             
-            led_display(play_step);
-            play_step++;
-            if (play_step > last_step) {
-                play_step = first_step;
-            }
-        }
+      //   if (active[ps]):
+      //       note_off(sequence[ps])
 
+  if (playing && delta > 60000000 / tempo) {
+    last_play_time = t;
+    led_display(play_step);
+    note_on(sequence[play_step]);
+    play_step++;
+    if (play_step > last_step) {
+        play_step = first_step;
     }
+  } else if (playing && delta > 30000000 / tempo and delta_off > 60000000 / tempo) {
+    last_off_time = t;
+    uint8_t ps = play_step - 1;
+    if (ps == 0xFF) {
+      ps = last_step;
+    }
+    note_off(sequence[ps]);
+  }
+
+    // The MIDI interface always creates input and output port/jack descriptors
+  // regardless of these being used or not. Therefore incoming traffic should be read
+  // (possibly just discarded) to avoid the sender blocking in IO
+  uint8_t packet[4];
+  while ( tud_midi_available() ) tud_midi_packet_read(packet);
 
 
-    return 0;
+
 }
 
 
+//--------------------------------------------------------------------+
+// Device callbacks
+//--------------------------------------------------------------------+
 
+// Invoked when device is mounted
+void tud_mount_cb(void)
+{
+
+}
+
+// Invoked when device is unmounted
+void tud_umount_cb(void)
+{
+
+}
+
+// Invoked when usb bus is suspended
+// remote_wakeup_en : if host allow us  to perform remote wakeup
+// Within 7ms, device must draw an average of current less than 2.5 mA from bus
+void tud_suspend_cb(bool remote_wakeup_en)
+{
+  (void) remote_wakeup_en;
+
+}
+
+// Invoked when usb bus is resumed
+void tud_resume_cb(void)
+{
+
+}
